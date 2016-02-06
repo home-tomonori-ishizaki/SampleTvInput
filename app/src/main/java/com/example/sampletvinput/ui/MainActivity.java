347c5629a7603b9d72d6ab29ffd07e7398660ba5
Tomonori Ishizaki <tomishi.android@gmail.com>
2016-02-06 21:48:22 +0900

    Create project and add empty activity

---
+++ app/src/main/java/com/example/sampletvinput/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.sampletvinput.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.sampletvinput.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/com/example/sampletvinput/ui/MainFragment.java
@@ -0,0 +1,47 @@
+package com.example.sampletvinput.ui;
+
+import android.os.Bundle;
+import android.support.v17.leanback.app.BrowseFragment;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.ClassPresenterSelector;
+import android.support.v17.leanback.widget.HeaderItem;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
+import android.util.Log;
+
+import com.example.sampletvinput.R;
+
+public class MainFragment extends BrowseFragment {
+    private static final String TAG = BrowseFragment.class.getSimpleName();
+    private ArrayObjectAdapter mRowAdapter;
+
+    public MainFragment() {
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.i(TAG, "onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+
+        setupUIElements();
+
+        loadRows();
+    }
+
+    private void setupUIElements() {
+        setTitle("Sample TV Input");
+
+        // over title
+        setHeadersState(HEADERS_ENABLED);
+        setHeadersTransitionOnBackEnabled(true);
+    }
+
+    private void loadRows() {
+        ClassPresenterSelector selector = new ClassPresenterSelector();
+        selector.addClassPresenter(ListRow.class, new ListRowPresenter());
+
+        mRowAdapter = new ArrayObjectAdapter(selector);
+
+        setAdapter(mRowAdapter);
+    }
+}
