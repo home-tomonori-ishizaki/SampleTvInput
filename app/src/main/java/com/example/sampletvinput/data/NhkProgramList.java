38c176e750e6ad9501e355f193f58b66e0d8337f
Tomonori Ishizaki <tomishi.android@gmail.com>
2016-02-11 10:09:07 +0900

    Use JsonIgnoreProperties

--- app/src/main/java/com/example/sampletvinput/data/NhkProgramList.java
+++ app/src/main/java/com/example/sampletvinput/data/NhkProgramList.java
@@ -1,25 +1,22 @@
 package com.example.sampletvinput.data;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class NhkProgramList {
     public Map<String, List<NhkProgram>> list;
 
+    @JsonIgnoreProperties(ignoreUnknown=true)
     public static class NhkProgram {
         public String id;
         public String event_id;
         public String start_time;
         public String end_time;
-        @JsonIgnore
-        public Object area;
-        @JsonIgnore
-        public Object service;
         public String title;
-        @JsonIgnore
-        public Object subtitle;
         public List<String> genres;
     }
 }
