3cee8c594dfad45026c6ccf3136c1c6f19f03fe7
Tomonori Ishizaki <tomishi.android@gmail.com>
2016-08-22 00:18:07 +0900

    Add dvr feature

--- app/src/main/java/com/example/sampletvinput/service/SampleInputService.java
+++ app/src/main/java/com/example/sampletvinput/service/SampleInputService.java
@@ -1,11 +1,17 @@
 package com.example.sampletvinput.service;
 
+import android.annotation.TargetApi;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.res.AssetFileDescriptor;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.media.MediaPlayer;
+import android.media.tv.TvContract;
 import android.media.tv.TvInputManager;
 import android.media.tv.TvInputService;
 import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 import android.view.Surface;
 
@@ -19,6 +25,12 @@ public class SampleInputService extends TvInputService {
     @Override
     public Session onCreateSession(String inputId) {
         return new SampleSessionImpl(this);
+    }
+
+    @Override
+    public RecordingSession onCreateRecordingSession(String inputId) {
+        Log.i(TAG, "onCreateRecordingSession(" + inputId + ")");
+        return new SampleRecordingSession(this, inputId);
     }
 
     /**
@@ -126,7 +138,93 @@ public class SampleInputService extends TvInputService {
             // {@link android.media.tv.TvInputManager#isRatingBlocked()}.
             // For the example implementation for the case, please see {@link RichTvInputService}.
             notifyContentAllowed();
+
+            // enable time shift feature(need to enable for recording feature)
+            if (Build.VERSION.SDK_INT >= 23) {
+                notifyTimeShiftStatusChanged(TvInputManager.TIME_SHIFT_STATUS_AVAILABLE);
+            }
+
             return true;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private class SampleRecordingSession extends RecordingSession {
+
+        private String mInputId;
+        private Uri mChannelUir;
+        private Uri mProgramUri;
+        private long mStartTime;
+        private long mEndTime;
+
+        public SampleRecordingSession(Context context, String inputId) {
+            super(context);
+            mInputId = inputId;
+        }
+
+        @Override
+        public void onTune(Uri uri) {
+            Log.i(TAG, "[DVR] onTune(" + uri + ")");
+            mChannelUir = uri;
+            notifyTuned(uri);
+        }
+
+        @Override
+        public void onStartRecording(Uri uri) {
+            mProgramUri = uri;
+            mStartTime = System.currentTimeMillis();
+            Log.i(TAG, "[DVR] onStartRecording(" + uri + ")");
+        }
+
+        @Override
+        public void onStopRecording() {
+            Log.i(TAG, "[DVR] onStopRecording(" + mProgramUri + ")");
+            mEndTime = System.currentTimeMillis();
+
+            Uri programUri = mProgramUri;
+            if (mProgramUri == null) {
+                // OTR case
+                long channelId = Long.valueOf(mChannelUir.getLastPathSegment());
+                programUri = TvContract.buildProgramsUriForChannel(channelId, mStartTime, mEndTime);
+            }
+
+            // get program info
+            Cursor cursor = getContentResolver().query(programUri,
+                    new String[]{
+                            TvContract.Programs.COLUMN_CHANNEL_ID,            // 0
+                            TvContract.Programs.COLUMN_TITLE,                 // 1
+                            TvContract.Programs.COLUMN_SHORT_DESCRIPTION,     // 2
+                            TvContract.Programs.COLUMN_START_TIME_UTC_MILLIS, // 3
+                            TvContract.Programs.COLUMN_END_TIME_UTC_MILLIS,   // 4
+                            TvContract.Programs.COLUMN_CANONICAL_GENRE,       // 5
+                            TvContract.Programs.COLUMN_POSTER_ART_URI         // 6
+                    }, null, null, null);
+            //DatabaseUtils.dumpCursor(cursor);
+
+            ContentValues values = new ContentValues();
+            if (cursor != null && cursor.moveToFirst()) {
+                values.put(TvContract.RecordedPrograms.COLUMN_CHANNEL_ID, cursor.getLong(0));
+                values.put(TvContract.RecordedPrograms.COLUMN_TITLE, cursor.getString(1));
+                values.put(TvContract.RecordedPrograms.COLUMN_SHORT_DESCRIPTION, cursor.getString(2));
+                values.put(TvContract.RecordedPrograms.COLUMN_START_TIME_UTC_MILLIS, mStartTime);
+                values.put(TvContract.RecordedPrograms.COLUMN_END_TIME_UTC_MILLIS, mEndTime);
+                //values.put(TvContract.RecordedPrograms.COLUMN_START_TIME_UTC_MILLIS, cursor.getLong(3));
+                //values.put(TvContract.RecordedPrograms.COLUMN_END_TIME_UTC_MILLIS, cursor.getLong(4));
+                values.put(TvContract.RecordedPrograms.COLUMN_CANONICAL_GENRE, cursor.getString(5));
+                values.put(TvContract.RecordedPrograms.COLUMN_POSTER_ART_URI, cursor.getString(6));
+            }
+
+            values.put(TvContract.RecordedPrograms.COLUMN_INPUT_ID, mInputId);
+            values.put(TvContract.RecordedPrograms.COLUMN_SEARCHABLE, 1);
+
+            getContentResolver().insert(TvContract.RecordedPrograms.CONTENT_URI, values);
+
+            notifyRecordingStopped(mProgramUri);
+        }
+
+        @Override
+        public void onRelease() {
+            Log.i(TAG, "[DVR] onRelease(" + mChannelUir + "," + mProgramUri + ")");
         }
     }
 }
